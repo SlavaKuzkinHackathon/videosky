fac28ec5571a19972e1eaedd25c47bdd968ceda3
SlavaKuzkinHackathon <52592175+SlavaKuzkinHackathon@users.noreply.github.com>
2021-02-04 15:29:59 +0700

    Update haiku.js

--- server/lib/haiku.js
+++ server/lib/haiku.js
@@ -1,6 +1,4 @@
 module.exports = () => {
-  const adj = adjs[Math.floor(Math.random() * adjs.length)];
-  const noun = nouns[Math.floor(Math.random() * nouns.length)];
   const MIN = 1000;
   const MAX = 9999;
   const num = Math.floor(Math.random() * ((MAX + 1) - MIN)) + MIN;
